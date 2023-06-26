5309d873bd94cc733d97ebc01c05f3813a757e51
Fabrizio Ervini <fabri.es018@gmail.com>
2023-06-26 16:22:52 +0200

    feat: create basic CRUD

---
+++ WebApi-REST/src/Movies.Api/Controllers/MoviesController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movies.Api.Mappers;
+using Movies.Application.Repositories;
+using Movies.Contracts.Requests;
+
+namespace Movies.Api.Controllers;
+
+[ApiController]
+public class MoviesController : ControllerBase
+{
+    private readonly IMovieRepository _movieRepository;
+
+    public MoviesController(IMovieRepository movieRepository)
+    {
+        _movieRepository = movieRepository;
+    }
+
+    [HttpPost(ApiEndpoints.Movies.Create)]
+    public async Task<IActionResult> Create([FromBody] CreateMovieRequest request)
+    {
+        var movie = request.MapToMovie();
+
+        await _movieRepository.CreateAsync(movie);
+
+        //return Created($"/{ApiEndpoints.Movies.Create}/{movie.Id}", movie);
+        return CreatedAtAction(nameof(Get), new { id = movie.Id }, movie);
+    }
+
+    [HttpGet(ApiEndpoints.Movies.GetAll)]
+    public async Task<IActionResult> GetAll()
+    {
+        var movies = await _movieRepository.GetAllAsync();
+
+        return Ok(movies.MapToResponse());
+    }
+
+    [HttpGet(ApiEndpoints.Movies.Get)]
+    public async Task<IActionResult> Get([FromRoute] Guid id)
+    {
+        var movie = await _movieRepository.GetByIdAsync(id);
+
+        if (movie is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(movie);
+    }
+
+    [HttpPut(ApiEndpoints.Movies.Update)]
+    public async Task<IActionResult> Update([FromRoute] Guid id, [FromBody] UpdateMovieRequest request)
+    {
+        var movie = request.MapToMovie(id);
+
+        var updatedMovie = await _movieRepository.UpdateAsync(movie);
+
+        if (!updatedMovie)
+        {
+            return NotFound();
+        }
+
+        return Ok(movie.MapToResponse());
+    }
+
+    [HttpDelete(ApiEndpoints.Movies.Delete)]
+    public async Task<IActionResult> Delete([FromRoute] Guid id)
+    {
+        var deleted = await _movieRepository.DeleteByIdAsync(id);
+
+        if (!deleted)
+        {
+            return NotFound();
+        }
+
+        return Ok();
+    }
+}
---
+++ WebApi-REST/src/Movies.Contracts/Responses/MoviesResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace Movies.Contracts.Responses;
+
+public class MoviesResponse
+{
+    public required IEnumerable<MovieResponse> Items { get; init; } = Enumerable.Empty<MovieResponse>();
+
+}
